ff204323dc72efa9de52f0d06a475f1fba9cb1ff
Shagor-Mia <infinityshagor18557@gmail.com>
2024-09-24 19:31:50 +0600

    3rd commit for jwt and logout in api

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -107,6 +107,8 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     .cookie("token", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+      samSite: "None",
+      secure: true,
     })
     .json({
       success: true,
